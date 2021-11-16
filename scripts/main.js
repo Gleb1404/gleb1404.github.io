6bdd60e9e70de47e1e4a7bce66fb6ed4e345fe5b
Gleb <gmuhach228@gmail.com>
2021-11-16 14:23:20 +0100

    adding my files to my repository

---
+++ scripts/main.js
@@ -0,0 +1,30 @@
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/sobaka.jpg') {
+      myImage.setAttribute ('src','images/sobaka2.jpg');
+    } else {
+      myImage.setAttribute ('src','images/sobaka.jpg');
+    }
+}
+
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h3');
+
+function setUserName() {
+  var myName = prompt('Укажите имя.');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'Собаки это круто, ' + myName;
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Собаки это круто, ' + storedName;
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
